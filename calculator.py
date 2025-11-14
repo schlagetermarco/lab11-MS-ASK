f628a3926b9f73992990cc406ae31dcc04667fa1
schla <schlagetermarco@gmail.com>
2025-11-14 16:53:40 -0500

    modified calculator partner 1

--- calculator.py
+++ calculator.py
@@ -4,8 +4,34 @@ calculator.py
 
 One function per operation, in order.
 """
+# GitHub link:https://github.com/schlagetermarco/lab11-MS-ASK
+# Partner 1 : Marco Schlageter
+
+import math
+
 # First example
-def add(a, b): 
-    pass
+def add(a, b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    if a==0:
+        raise ZeroDivisionError("Can't Divide by 0")
+    return b/a
+
+def logarithm(a,b):
+    if a<=0 or a==1 or b<=0:
+        raise ValueError("Invalid inputs for logarithmic function")
+    return math.log(b,a)
+
+def exponent(a,b):
+    return a**b
+
+
 
 
