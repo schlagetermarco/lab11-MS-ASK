3d0ec3e204f68d4a51e12a96aeadeed7f4049f25
Ask79 <akodavati399@gmail.com>
2025-11-15 19:03:35 -0500

    Partner 2 Part 1 functions added

--- calculator.py
+++ calculator.py
@@ -32,6 +32,25 @@ def logarithm(a,b):
 def exponent(a,b):
     return a**b
 
+# Partner 2 functions: Akash K
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Can't Divide by 0")
+    return b / a
+
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("Invalid inputs for logarithmic function")
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
 
 
 
